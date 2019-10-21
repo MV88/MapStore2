5a564cb9fa90c1fb3d3220acdc656e393ed177c5
Matteo Velludini <matteo.velludini@geo-solutions.it>
2019-10-21 14:35:42 +0200

    components cookie

--- web/client/components/cookie/Cookie.jsx
+++ web/client/components/cookie/Cookie.jsx
@@ -5,11 +5,12 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
 */
-const PropTypes = require('prop-types');
-const React = require('react');
-const {Button, Glyphicon, Col} = require('react-bootstrap');
-const Message = require('../../components/I18N/Message');
-const MoreDetails = require('./MoreDetails');
+import PropTypes from 'prop-types';
+
+import React from 'react';
+import { Button, Glyphicon, Col } from 'react-bootstrap';
+import Message from '../../components/I18N/Message';
+import MoreDetails from './MoreDetails';
 /**
   * Component used to show a panel with the information about cookies
   * @class Cookies
